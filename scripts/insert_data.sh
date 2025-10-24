d60bec814943be4c86b1e0e57748f42854de988d
CodyBense <codybense@gmail.com>
2025-10-24 17:03:56 -0400

    UPDATE: fixed inset statements

--- scripts/insert_data.sh
+++ scripts/insert_data.sh
@@ -2,108 +2,108 @@
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Lemon Garlic Butter Beans", "", "", "", "20", "", "https://shortgirltallorder.com/creamy-lemon-garlic-butter-beans", "");"
+    VALUES ('Lemon Garlic Butter Beans', '', '', '', 20, 0, 'https://shortgirltallorder.com/creamy-lemon-garlic-butter-beans', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Shrimp and Spinch Stuffed Pasta Rolls", "", "", "", "50", "", "https://recipeamelia.com/shrimp-and-spinach-stuffed-pasta-rolls/", "");"
+    VALUES ('Shrimp and Spinch Stuffed Pasta Rolls', '', '', '', 50, 0, 'https://recipeamelia.com/shrimp-and-spinach-stuffed-pasta-rolls/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("French Onion Pasta", "", "", "", "50", "", "https://www.modernhoney.com/one-pot-french-onion-pasta/#wprm-recipe-container-43667", "");"
+    VALUES ('French Onion Pasta', '', '', '', 50, 0, 'https://www.modernhoney.com/one-pot-french-onion-pasta/#wprm-recipe-container-43667', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Tomato Soup with Cheese Dumplings", "", "", "", "50", "", "https://lizarecipes.com/tomato-soup-with-cheese-dumplings/", 2025-10-23"");"
+    VALUES ('Tomato Soup with Cheese Dumplings', '', '', '', 50, 0, 'https://lizarecipes.com/tomato-soup-with-cheese-dumplings/', '2025-10-23');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Baked Feta Gnocchi", "", "", "", "45", "", "https://www.littlehomeinthemaking.com/baked-feta-gnocchi/#mv-creation-182-jtr", "");"
+    VALUES ('Baked Feta Gnocchi', '', '', '', 45, 0, 'https://www.littlehomeinthemaking.com/baked-feta-gnocchi/#mv-creation-182-jtr', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Finnish Salmon Soup", "", "", "", "30", "", "https://lacuisinedegeraldine.fr/en/finnish-salmon-soup-lohikeitto", "");"
+    VALUES ('Finnish Salmon Soup', '', '', '', 30, 0, 'https://lacuisinedegeraldine.fr/en/finnish-salmon-soup-lohikeitto', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Creamy Pumpkin Ravioli", "", "", "", "20", "", "https://moderncrumb.com/creamy-pumpkin-ravioli/#mv-creation-103-jtr", "");"
+    VALUES ('Creamy Pumpkin Ravioli', '', '', '', 20, 0, 'https://moderncrumb.com/creamy-pumpkin-ravioli/#mv-creation-103-jtr', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Cheesy Pumpkin Ricotta Pasta Bake", "", "", "", "60", "", "https://dishesrecipe.com/recipe/cheesy-pumpkin-ricotta-pasta-bake/", "");"
+    VALUES ('Cheesy Pumpkin Ricotta Pasta Bake', '', '', '', 60, 0, 'https://dishesrecipe.com/recipe/cheesy-pumpkin-ricotta-pasta-bake/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Marry Me Salmon", "", "", "", "25", "", "https://sweetbananachef.com/marry-me-salmon-the-ultimate-recipe-for-a-romantic-dinner/", "");"
+    VALUES ('Marry Me Salmon', '', '', '', 25, 0, 'https://sweetbananachef.com/marry-me-salmon-the-ultimate-recipe-for-a-romantic-dinner/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Baked Ziti", "", "", "", "70", "", "https://recipecakes.com/best-ever-baked-ziti-recipe-comfort-food-delight/", "");"
+    VALUES ('Baked Ziti', '', '', '', 70, 0, 'https://recipecakes.com/best-ever-baked-ziti-recipe-comfort-food-delight/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Sweet and Spicy Hot Honey Garlic Bread Pizza", "", "", "", "25", "", "https://recipesinsight.com/hot-honey-garlic-bread-pizza-recipe/", "");"
+    VALUES ('Sweet and Spicy Hot Honey Garlic Bread Pizza', '', '', '', 25, 0, 'https://recipesinsight.com/hot-honey-garlic-bread-pizza-recipe/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Alfredo Sauce", "", "", "", "10", "", "https://feedingyourfam.com/alfredo/#wprm-recipe-container-83030", "");"
+    VALUES ('Alfredo Sauce', '', '', '', 10, 0, 'https://feedingyourfam.com/alfredo/#wprm-recipe-container-83030', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Creamy Spinach Tomato Tortellini", "", "", "", "10", "", "https://familypanflavors.com/creamy-spinach-tomato-tortellini/", "");"
+    VALUES ('Creamy Spinach Tomato Tortellini', '', '', '', 10, 0, 'https://familypanflavors.com/creamy-spinach-tomato-tortellini/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Pesto Grilled Cheese", "", "", "", "20", "", "https://mollyshomeguide.com/sourdough-pesto-grilled-cheese-73986/", "");"
+    VALUES ('Pesto Grilled Cheese', '', '', '', 20, 0, 'https://mollyshomeguide.com/sourdough-pesto-grilled-cheese-73986/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Red Pepper Alfredo", "", "", "", "10", "", "https://chocolatecoveredkatie.com/red-pepper-alfredo-pasta-recipe/", "");"
+    VALUES ('Red Pepper Alfredo', '', '', '', 10, 0, 'https://chocolatecoveredkatie.com/red-pepper-alfredo-pasta-recipe/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Salmon Pasta", "", "", "", "20", "", "https://www.pinterest.com/pin/588493876342185888/", "");"
+    VALUES ('Salmon Pasta', '', '', '', 20, 0, 'https://www.pinterest.com/pin/588493876342185888/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Roasted Tomato and Garlic Ricotta Pasta", "", "", "", "35", "", "https://coolcookingrecipes.com/recipe/roasted-tomato-and-garlic-ricotta-pasta/", "");"
+    VALUES ('Roasted Tomato and Garlic Ricotta Pasta', '', '', '', 35, 0, 'https://coolcookingrecipes.com/recipe/roasted-tomato-and-garlic-ricotta-pasta/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Miso Udon", "", "", "", "30", "", "https://thefoodietakesflight.com/miso-udon-noodle-soup/#recipe", "");"
+    VALUES ('Miso Udon', '', '', '', 30, 0, 'https://thefoodietakesflight.com/miso-udon-noodle-soup/#recipe', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Miso Glazed Salmon", "", "", "", "10", "", "https://www.pinterest.com/pin/588493876339298716/", "");"
+    VALUES ('Miso Glazed Salmon', '', '', '', 10, 0, 'https://www.pinterest.com/pin/588493876339298716/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Sun-Dried Tomato Pasta", "", "", "", "30", "", "https://www.saltandlavender.com/sun-dried-tomato-pasta/#wprm-recipe-container-59829", "");"
+    VALUES ('Sun-Dried Tomato Pasta', '', '', '', 30, 0, 'https://www.saltandlavender.com/sun-dried-tomato-pasta/#wprm-recipe-container-59829', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Garlic Butter Parmesan Fried Gnocchi", "", "", "", "15", "", "https://www.alainga.com/garlic-butter-parmesan-fried-gnocchi/", "");"
+    VALUES ('Garlic Butter Parmesan Fried Gnocchi', '', '', '', 15, 0, 'https://www.alainga.com/garlic-butter-parmesan-fried-gnocchi/', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Shashuka", "", "", "", "20", "", "", "");"
+    VALUES ('Shashuka', '', '', '', 20, 0, '', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Lemon Salmon Orzo", "", "", "", "35", "", "https://tonisrecipes.com/lemon-salmon-orzo/#wprm-recipe-container-9422", "");"
+    VALUES ('Lemon Salmon Orzo', '', '', '', 35, 0, 'https://tonisrecipes.com/lemon-salmon-orzo/#wprm-recipe-container-9422', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Salmon and Asparagus Foil Packs with Garlic Butter Sauce", "", "", "", "25", "", "https://www.eatwell101.com/baked-salmon-asparagus-foil-packs-recipe", "");"
+    VALUES ('Salmon and Asparagus Foil Packs with Garlic Butter Sauce', '', '', '', 25, 0, 'https://www.eatwell101.com/baked-salmon-asparagus-foil-packs-recipe', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Creamy Spinach and Mushroom Gnocchi", "", "", "", "20", "", "https://juliasalbum.com/spinach-mushroom-gnocchi/#recipe", "");"
+    VALUES ('Creamy Spinach and Mushroom Gnocchi', '', '', '', 20, 0, 'https://juliasalbum.com/spinach-mushroom-gnocchi/#recipe', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Stuffed Peppers", "", "", "", "90", "", "https://www.delish.com/cooking/recipe-ideas/a23014857/classic-stuffed-peppers-recipe/?epik=dj0yJnU9VXZDSXF3dWRWdUVJX2xXYmh4ZzNxTlBoaWY2OTgzSEcmcD0wJm49dUNYRTllOURJOGwtQ1Y5TmZNS2ZRUSZ0PUFBQUFBR2o3MDNj", "");"
+    VALUES ('Stuffed Peppers', '', '', '', 90, 0, 'https://www.delish.com/cooking/recipe-ideas/a23014857/classic-stuffed-peppers-recipe/?epik=dj0yJnU9VXZDSXF3dWRWdUVJX2xXYmh4ZzNxTlBoaWY2OTgzSEcmcD0wJm49dUNYRTllOURJOGwtQ1Y5TmZNS2ZRUSZ0PUFBQUFBR2o3MDNj', '');"
 
 sqlite3 sqlite/recipes.db "INSERT INTO recipes \
     (name, cuisine_type, flavor, difficulty, time, liked, link, last_used) \
-    VALUES ("Creamy Roasted Pepper Pasta", "", "", "", "", "", "https://www.pinterest.com/pin/588493876337800518/", "");"
+    VALUES ('Creamy Roasted Pepper Pasta', '', '', '', 0, 0, 'https://www.pinterest.com/pin/588493876337800518/', '');"
