a29a2c3c9064f42528b10ea81de321b373fa085e
CodyBense <codybense@gmail.com>
2025-10-24 22:21:57 -0400

    ADDED: GetID and UpdateLiked funcs

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/glebarez/go-sqlite"
@@ -20,9 +21,9 @@ type Recipe struct {
 }
 
 func FindAll(db *sql.DB) ([]Recipe, error) {
-	sql := "SELECT * FROM recipes"
+	query := "SELECT * FROM recipes"
 
-	rows, err := db.Query(sql)
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatalf("Find all query failed: %s\n", err)
 		return nil, err
@@ -35,11 +36,74 @@ func FindAll(db *sql.DB) ([]Recipe, error) {
 		r := &Recipe{}
 		err := rows.Scan(&r.Id, &r.Name, &r.Cusine_Type, &r.Flavor, &r.Difficulty, &r.Time, &r.Liked, &r.Link, &r.Last_Used)
 		if err != nil {
-			log.Fatalf("Database row scan failed: %s\n", err)
+			log.Fatalf("FindAll row scan failed: %s\n", err)
 			return nil, err
 		}
 		recipes = append(recipes, *r)
 	}
 
 	return recipes, nil
+}
+
+func GetID(db *sql.DB, recipeName string) int {
+	query := fmt.Sprintf("SELECT id FROM recipes WHERE name = '%s'", recipeName)
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatalf("GetID query failed: %s\n", err)
+		return 0
+	}
+	defer rows.Close()
+
+	var recipes []Recipe
+
+	for rows.Next() {
+		r := &Recipe{}
+		err := rows.Scan(&r.Id)
+		if err != nil {
+			log.Fatalf("GetID row scan failed: %s\n", err)
+		}
+		recipes = append(recipes, *r)
+	}
+
+	return recipes[0].Id
+}
+
+func UpdateLiked(db *sql.DB, recipeID int) {
+	getQuery := fmt.Sprintf("SELECT liked FROM recipes WHERE id = '%d'", recipeID)
+
+	rows, err := db.Query(getQuery)
+	if err != nil {
+		log.Fatalf("Upate getQuery failed: %s\n", err)
+	}
+	defer rows.Close()
+
+	var recipes []Recipe
+
+	for rows.Next() {
+		r := &Recipe{}
+		err := rows.Scan(&r.Liked)
+		if err != nil {
+			log.Fatalf("Upate getQuery row scan failed: %s\n", err)
+		}
+
+		recipes = append(recipes, *r)
+	}
+
+	if recipes[0].Liked == false {
+		updateQuery := fmt.Sprintf("UPDATE recipes SET liked = 1 WHERE id = %d", recipeID)
+
+		_, err := db.Exec(updateQuery)
+		if err != nil {
+			log.Fatalf("UpdateLiked query failed: %s\n", err)
+		}
+	} else {
+		updateQuery := fmt.Sprintf("UPDATE recipes SET liked = 0 WHERE id = %d", recipeID)
+
+		_, err := db.Exec(updateQuery)
+		if err != nil {
+			log.Fatalf("UpdateLiked query failed: %s\n", err)
+		}
+	}
+
 }
