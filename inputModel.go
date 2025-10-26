535ce07404ac75926f280645002d81145e0477cc
CodyBense <codybense@gmail.com>
2025-10-26 07:45:14 -0400

    UPDATE: changed tableModel and inputModel to TableModel and InputModel

--- inputModel.go
+++ inputModel.go
@@ -5,7 +5,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type inputModel struct {
+type InputModel struct {
 	id           textinput.Model
 	name         textinput.Model
 	cuisine_type textinput.Model
@@ -17,9 +17,9 @@ type inputModel struct {
 	last_used    textinput.Model
 }
 
-func (m inputModel) Init() tea.Cmd { return nil }
+func (m InputModel) Init() tea.Cmd { return nil }
 
-func (m inputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m InputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -32,6 +32,32 @@ func (m inputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m inputModel) View() string {
+func (m InputModel) View() string {
 	return "\n"
+}
+
+func NewInput(recipes *TableModel) InputModel {
+	input := InputModel{
+		id: textinput.New(),
+		name: textinput.New(),
+		cuisine_type: textinput.New(),
+		flavor: textinput.New(),
+		difficulty: textinput.New(),
+		time: textinput.New(),
+		liked: textinput.New(),
+		link: textinput.New(),
+		last_used: textinput.New(),
+	}
+
+	input.id.Placeholder = recipes.table.SelectedRow()[0]
+	input.name.Placeholder = recipes.table.SelectedRow()[1]
+	input.cuisine_type.Placeholder = recipes.table.SelectedRow()[2]
+	input.flavor.Placeholder = recipes.table.SelectedRow()[3]
+	input.difficulty.Placeholder = recipes.table.SelectedRow()[4]
+	input.time.Placeholder = recipes.table.SelectedRow()[5]
+	input.liked.Placeholder = recipes.table.SelectedRow()[7]
+	input.link.Placeholder = recipes.table.SelectedRow()[8]
+	input.last_used.Placeholder = recipes.table.SelectedRow()[9]
+
+	return input
 }
